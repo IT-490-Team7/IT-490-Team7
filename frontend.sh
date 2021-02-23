42dd252e05ec05b72ded395fd015bbbc6780bef7
skp65 <60273787+skp65@users.noreply.github.com>
2021-02-23 18:16:59 -0500

    front end sciprt

---
+++ frontend.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+STATUS="$(systemctl is-active apache2.service)"
+serv=Apache
+
+if [ "${STATUS}" = "active" ]; then
+    echo "$serv is running....."
+else 
+    echo " Service not running.... and will start up shortly"
+    systemctl start apache2
+    echo "Checking status..."
+    systemctl status apache2
+    exit 1  
+fi
