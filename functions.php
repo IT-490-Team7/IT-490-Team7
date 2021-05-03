0602d5eff672d2fdac2f529c29743158fea45505
Neel2398 <54948373+Neel2398@users.noreply.github.com>
2021-05-03 13:16:11 -0400

    Add files via upload

--- functions.php
+++ functions.php
@@ -1,22 +1,10 @@
 <?php
-
-include('../db/config.php');
-///function for safe insertion into the database without errors
-function escape($string){
-    global $con;
-
-    return mysqli_real_escape_string($string);
-
-}
-
-
-///this function checks if email exists in the database return true or false
 function email_exist($email){
-    global $con;
+    global $db;
 
     $sql_email = "SELECT * FROM users WHERE email = '$email'";
 
-    $sql_query = mysqli_query($con, $sql_email);
+    $sql_query = mysqli_query($db, $sql_email);
 
     $sql_rows =  mysqli_num_rows($sql_query);
 
@@ -30,9 +18,7 @@ function email_exist($email){
     }
 }
 
-//This function checks for the length of the password it must be 6 or more return true or false
 function check_pass_length($password){
-    global $con;
 
     if (strlen($password) >= 6){
 
@@ -44,34 +30,3 @@ function check_pass_length($password){
     }
 
 }
-
-/*function register($name, $email, $password){
-    global $con;
-
-    $name    = escape($name);
-    $email    = escape($email);
-    $password = escape($password);
-
-
-    if (email_exist($email)){
-
-        return false;
-
-    }elseif (!check_pass_length($password)){
-
-        return false;
-
-    }else{
-
-        $password = password_hash($password, PASSWORD_DEFAULT);
-
-        $insert_user = "INSERT INTO users (name, email, password) VALUES ('$name','$email','$password')";
-
-        mysqli_query($con, $insert_user);
-
-        return true;
-
-    }
-
-}
-*/
